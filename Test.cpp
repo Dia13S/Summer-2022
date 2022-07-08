e976f277f630f034a7be1a65d3c1cfd9f251f3b8
Diana <90005572+Dia13S@users.noreply.github.com>
2022-07-08 08:37:11 +0300

    Create Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int main () {
+
+string line;
+
+ifstream in("/C:\Users/Diana/Desctop/colors.jpg");
+
+if (in.is_open()){
+    while (getline(in, line)){
+        cout << line << endl;
+    }
+}
+in.close();
+
+return 0;
+
+ }
